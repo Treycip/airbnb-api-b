de3f9270d79c8037bbd6dc77001dead297321c4f
Treycip <91564051+Treycip@users.noreply.github.com>
2023-12-15 16:54:23 -0500

    commit2

--- src/app/airbnb/route.ts
+++ src/app/airbnb/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse, NextRequest } from "next/server";
-import { places } from "@/mock/place";
+import { places } from "@/mock/places";
 
 export function GET(req: NextRequest){
     return NextResponse.json(places)
